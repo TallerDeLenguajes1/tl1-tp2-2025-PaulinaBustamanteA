3636ab827acec0e7308002a708edb8618adf4f56
Tatiana Paulina Bustamante Arias <paulinaarias2205@gmail.com>
2025-04-21 19:34:41 -0300

    función CargarCaracteristicasCompu

--- misFunciones.h
+++ misFunciones.h
@@ -10,3 +10,22 @@ struct Compu
     int cantidad_nucleos; // Cantidad de núcleos (valor entre 1 y 8)
     char *tipo_cpu;       // Tipo de procesador (apuntador a cadena de caracteres)
 } typedef Compu;
+
+// Declaracion
+void CargarCaracteristicasCompu(Compu *computadoras, int cantidad_pc);
+
+
+// Definicion
+void CargarCaracteristicasCompu(Compu *computadoras, int cantidad_pc)
+{
+    int index_tipo = 0;
+    for (int i = 0; i < cantidad_pc; i++)
+    {
+        computadoras[i].velocidad = 1 + rand() % 3;
+        // a + rand()%(b-a + 1)
+        computadoras[i].anio = 2015 + rand() % (2024 - 2015 + 1);
+        computadoras[i].cantidad_nucleos = 1 + rand() % 8;
+        index_tipo = rand() % 6;
+        computadoras[i].tipo_cpu = tipos[index_tipo];
+    }
+}
